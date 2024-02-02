775fcaa9e338a388a7d8dd98e7dd1833ea2bc519
Deviprasadhack <85698605+Deviprasadhack@users.noreply.github.com>
2024-02-02 11:20:43 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,22 @@
+import requests
+import telebot
+
+bot = telebot.TeleBot('6121118235:AAGdNmeJapt2o0iRJTvsRz56zx6uNIdKXOQ') 
+
+@bot.message_handler(commands=['generate_qr'])  
+def generate_qr(message):
+
+  text = message.text.split(' ', 1)[1]
+  
+  api_url = f"https://api.qrserver.com/v1/create-qr-code/?size=150x150&data={text}"  
+
+  response = requests.get(api_url)
+  qr_image = response.content
+  
+  bot.send_photo(message.chat.id, qr_image)
+
+
+  #Call API to read QR code
+  #Send result to user  
+
+  bot.polling()
